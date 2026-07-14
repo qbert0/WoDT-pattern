cf70adad662dd845acbedf9269340276f723362d
NGUYENDUONG5G <apeach2901@gmail.com>
2026-07-14 09:03:28 +0700

    sửa config

--- client-aplication/vite.config.js
+++ client-aplication/vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'rewrite-dt-routes',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url && (req.url.startsWith('/dt/') || req.url.includes('/dt/'))) {
+            req.url = '/index.html';
+          }
+          next();
+        });
+      }
+    }
+  ],
 })
